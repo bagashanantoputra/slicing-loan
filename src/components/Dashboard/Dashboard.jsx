c9dff9168aecb70982dcfbf52d90c3b56c01cede
bagashanantoputra <danandjaya123@gmail.com>
2023-10-13 11:17:09 +0700

    responsive mobile

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -85,7 +85,7 @@ export default function Dashboard() {
                             <div className="flex-row lg:w-1/2">
                                 <div className="lg:flex">
                                     {ChartDataCostumer.map((item) => (
-                                    <Card key={item.id} className="ring-0 ring-transparent shadow-transparent py-0 px-5" decoration="left" decorationColor={item.color}>
+                                    <Card key={item.id} className="ring-0 ring-transparent shadow-transparent py-0 px-5 bg-white" decoration="left" decorationColor={item.color}>
                                         <div className="flex justify-between items-center lg:mb-0 mb-5">
                                             <div>
                                                 <Text className="text-xs">{item.name}</Text>
@@ -113,16 +113,16 @@ export default function Dashboard() {
                             </div>
                             
                             <div className="flex-row lg:w-1/2">
-                                <div className="flex w-64">
+                                <div className="flex">
                                     {ChartData.map((item) => (
-                                    <Card key={item.id} className="mx-auto ring-0 ring-transparent shadow-transparent py-0 px-5" decoration="left" decorationColor={item.color}>
+                                    <Card key={item.id} className="mx-auto ring-0 ring-transparent shadow-transparent py-0 px-5 bg-white" decoration="left" decorationColor={item.color}>
                                         <div className="flex justify-between items-center">
                                             <div>
                                                 <Text className="text-xs">{item.name}</Text>
                                                 <Metric className="mt-2 text-xl">{item.countData}</Metric>
                                             </div>
                                             <div>
-                                                <svg className="ps-8 lg:pe-10" width="190" height="70">
+                                                <svg className="ps-8 lg:pe-0 lg:ps-0" width="190" height="70">
                                                     <path d="M0 40 Q25 25, 50 50 T100 50 T150 50 T200 10" stroke={item.color} fill="none"/>
                                                 </svg>
                                             </div>
